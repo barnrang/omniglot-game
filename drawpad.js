fac72dd04307db2533d91a26dfa039896a7b8592
barnrang <barnrang@gmail.com>
2019-10-02 19:43:45 +0900

    change event listener

--- drawpad.js
+++ drawpad.js
@@ -5,6 +5,7 @@ var sketch = document.getElementById('sketch');
 var sketch_style = getComputedStyle(sketch);
 canvas.width = 420;
 canvas.height = 420;
+mouse_listening = 0;
 
 var mouse = {x: 0, y: 0};
  
@@ -12,6 +13,13 @@ var mouse = {x: 0, y: 0};
 canvas.addEventListener('mousemove', function(e) {
   mouse.x = e.pageX - this.offsetLeft;
   mouse.y = e.pageY - this.offsetTop;
+  onPaint();
+}, false);
+
+canvas.addEventListener('ondrag', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+  onPaint();
 }, false);
 
 /* Drawing on Paint App */
@@ -28,17 +36,44 @@ function getSize(size){ctx_pad.lineWidth = size;}
 //ctx.strokeStyle = document.settings.colour[1].value;
  
 canvas.addEventListener('mousedown', function(e) {
+    mouse_listening = 1;
     ctx_pad.beginPath();
     ctx_pad.moveTo(mouse.x, mouse.y);
- 
-    canvas.addEventListener('mousemove', onPaint, false);
+    console.log(mouse_listening);
+    console.log('mouse down');
+    
 }, false);
+
+// canvas.addEventListener('mousemove', onPaint, false);
  
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
+window.addEventListener('mouseup', function() {
+      mouse_listening = 0;
+      // canvas.removeEventListener('mousemove', onPaint, false);
+      console.log(mouse_listening);
+      console.log('mouse up');
+    
 }, false);
+
+// canvas.addEventListener('mouseleave', function() {
+//   mouse_listening = 0;
+//   // canvas.removeEventListener('mousemove', onPaint, false);
+//   console.log(mouse_listening);
+//   console.log('mouse leave');
+
+// }, false);
+
+// canvas.addEventListener('mouseleave', function() {
+//   if (mouse_listening) {
+//     canvas.removeEventListener('mousemove', onPaint, false);
+//     mouse_listening = 0;
+//   }
+//   console.log(mouse_listening);
+// }, false);
  
 var onPaint = function() {
-    ctx_pad.lineTo(mouse.x, mouse.y);
-    ctx_pad.stroke();
+  console.log('drawing');
+    if (mouse_listening) {
+      ctx_pad.lineTo(mouse.x, mouse.y);
+      ctx_pad.stroke();
+    }
 };
